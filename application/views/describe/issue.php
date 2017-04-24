d1dd99c051892ad5ce810249c6d7832d9e19842c
suresh07 <soori_rozee@yahoo.com>
2017-04-24 12:52:15 +0530

    UI fixes

--- application/views/describe/issue.php
+++ application/views/describe/issue.php
@@ -1,11 +1,13 @@
 <div class="archive_title">ಸಂಚಿಕೆ <span class="eng">(<?= intval($data[0]->issue); ?>)</span> - <?= $viewHelper->monthNames[$data[0]->month] ?> <br />ಸಂಪುಟ <span class="eng">(<?= intval($data[0]->volume) ?>)</span> - <span class="eng"><?= $data[0]->year ?></span></div>
 	<div class="scroll">
-		<?php foreach($data as $row): ?>
-			<li><span class="titlespan"><a href="../Volumes/$volume/$issue/index.djvu?djvuopts&page=$page&zoom=page" target="_blank"><?= $row->title ?></a></span>
-			<?php if($row->authid != 0): ?>
-				<span><?= $viewHelper->getAuthorDetails($row->authorDetails); ?></span>
-			<?php endif; ?>
-			<span class="downloadPdf">( <a href="<?=BASE_URL ?>article/download/<?= $row->ID?>" target="_blank">ಡೌನ್ಲೋಡ್ ಪಿಡಿಎಫ್</a> )</span>
-		<?php endforeach;?>
+		<ul>
+			<?php foreach($data as $row): ?>
+				<li><span class="titlespan"><a href="../Volumes/$volume/$issue/index.djvu?djvuopts&page=$page&zoom=page" target="_blank"><?= $row->title ?></a></span>
+				<?php if($row->authid != 0): ?>
+					<span><?= $viewHelper->getAuthorDetails($row->authorDetails); ?></span>
+				<?php endif; ?>
+				<span class="downloadPdf">( <a href="<?=BASE_URL ?>article/download/<?= $row->ID?>" target="_blank">ಡೌನ್ಲೋಡ್ ಪಿಡಿಎಫ್</a> )</span>
+			<?php endforeach;?>
+		</ul>
 	</div>
 </div>
--- application/views/listing/articles.php
+++ application/views/listing/articles.php
@@ -40,13 +40,15 @@
 	<span <?php if($data[0]->character == 'ಹ'): ?> class="searchletter" <?php else: ?>class="alphaspan"> <?php endif;?><a href="<?=BASE_URL ?>listing/articles/ಹ">&nbsp;ಹ</a>&nbsp;</span>
 </div>
 <div class="scroll">
-	<?php foreach($data as $row): ?>
-		<li><span class="titlespan"><a href="" target="_blank"><?= $row->title ?></a></span>
-		<?php if($row->authid != 0): ?>
-			<span><?= $viewHelper->getAuthorDetails($row->authorDetails); ?></span>
-		<?php endif; ?>
-		<br />&nbsp;&nbsp;&nbsp;&nbsp;<span class="yearspan"><a href="javascript:void();">ಸಂಪುಟ&nbsp;<span class="eng"><?= intval($row->volume); ?></span></a></span>&nbsp;<span class="yearspan"><a href="javascript:void();"><?= $viewHelper->monthNames[$row->month] ?> <span class="eng">(<?= intval($row->year);?>)</span></a> | </span>
-		<span class="downloadPdf">(<a href="<?=BASE_URL ?>article/download/<?= $row->ID?>" target="_blank">ಡೌನ್ಲೋಡ್ ಪಿಡಿಎಫ್</a>)</span></li>
-	<?php endforeach;?>
+	<ul>
+		<?php foreach($data as $row): ?>
+			<li><span class="titlespan"><a href="" target="_blank"><?= $row->title ?></a></span>
+			<?php if($row->authid != 0): ?>
+				<span><?= $viewHelper->getAuthorDetails($row->authorDetails); ?></span>
+			<?php endif; ?>
+			<br />&nbsp;&nbsp;&nbsp;&nbsp;<span class="yearspan"><a href="javascript:void();">ಸಂಪುಟ&nbsp;<span class="eng"><?= intval($row->volume); ?></span></a></span>&nbsp;<span class="yearspan"><a href="javascript:void();"><?= $viewHelper->monthNames[$row->month] ?> <span class="eng">(<?= intval($row->year);?>)</span></a> | </span>
+			<span class="downloadPdf">(<a href="<?=BASE_URL ?>article/download/<?= $row->ID?>" target="_blank">ಡೌನ್ಲೋಡ್ ಪಿಡಿಎಫ್</a>)</span></li>
+		<?php endforeach;?>
+	</ul>
 </div>
 	
