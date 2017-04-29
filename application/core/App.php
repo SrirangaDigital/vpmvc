953bc8fef8f655582481f9e06f73f657bd96a3bc
suresh07 <soori_rozee@yahoo.com>
2017-04-29 16:26:25 +0530

    Removed Unwanted functions

--- application/core/App.php
+++ application/core/App.php
@@ -50,7 +50,7 @@ class App{
 		}
 		else{
 			// If url does not have any parameters, then it is assumed that it will be the home page and hence 'Home' is returned 
-			return array('data', 'getData');
+			return array('Home');
 		}
 	}
 
